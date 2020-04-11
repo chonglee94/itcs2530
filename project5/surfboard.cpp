4edf61c14920c60b6b992a09ff5821d6050f492c
Chong Lee <me@chonglee.com>
2020-04-11 14:25:32 -0400

    surfboard project 5

---
+++ project5/surfboard.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void ShowUsage(){
+  cout << "To show program usage 'S'" << endl;
+  cout << "To purchase a surfboard press 'P'" << endl;
+  cout << "To display current purchases press 'C'" << endl;
+  cout << "To display total amount due press 'T'" << endl;
+  cout << "To quit the program press 'Q'" << endl;
+}
+
+int main() {
+
+  int iTotalSmall, iTotalMedium, iTotalLarge;
+  char select = 0;
+
+  cout << "To show program usage 'S'" << endl;
+  cout << "To purchase a surfboard press 'P'" << endl;
+  cout << "To display current purchases press 'C'" << endl;
+  cout << "To display total amount due press 'T'" << endl;
+  cout << "To quit the program press 'Q'" << endl;
+  cout << "\n\nPlease enter selection: "; cin >> select;
+
+  while
+
+  return(0);
+}
